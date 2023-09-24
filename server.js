01637ce8a0d78707f1dca489b9183c943cd9b588
ktortolini <134340991+ktortolini@users.noreply.github.com>
2023-09-24 14:38:18 -0500

    added the post routes for user and score to the server js file

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 // imports the basic modules
+const fs = require('fs');
 const path = require('path');
+const uniqId = require('uniqid');
 const express = require('express');
+const session = require('express-session');
 // imports the middleware module
 const { middleware } = require('./middleware/middleware');
+// imports models for score and user
+const { User, Score } = require('./models');
+// imports the sequelize connection
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -33,9 +41,60 @@ app.get('/trainer', function (req, res) {
 	res.sendFile(path.join(__dirname, './public/trainer.html'));
 });
 
-// get route for api
+// post route for user
+app.post('/user', async function (req, res) {
+	try {
+		const userData = await User.create({
+			name: req.body.name,
+		});
+		// sets the variables for the session
+		req.session.save(() => {
+			req.session.user_id = userData.id;
+			req.session.user_name = userData.name;
+			req.session.logged_in = true;
+		});
+		// returns the userData
+		res.status(200).json(userData);
+	} catch (err) {
+		res.status(400).json(err);
+		// this is for debugging purposes
+		console.log(`catch (${err.message})`);
+	}
+});
+
+// get routes for api code and scores
 app.get('/api/code', function (req, res) {
-	res.sendFile(path.join(__dirname, './db/code.json'));
+	res.sendFile(path.join(__dirname, './db/json/code.json'));
+});
+
+app.get('/api/scores', (req, res) => {
+	res.sendFile(path.join(__dirname, './db/json/scores.json'));
+});
+
+// post route for api scores
+app.post('/api/scores', function (req, res) {
+	// creates a new score
+	let data = [];
+	const score = req.body;
+	try {
+		// reads and parses the database file
+		data = JSON.parse(fs.readFileSync('./db/json/scores.json', 'utf8'));
+	} catch (err) {
+		// this is for debugging purposes
+		console.log(`catch (${err.message})`);
+	}
+	// this is for debugging purposes
+	console.log(`const data = ${JSON.stringify(data)}`);
+	console.log(`const score = ${JSON.stringify(req.body)}`);
+	// adds a unique id
+	score.id = uniqId();
+	// pushes to array
+	data.push(score);
+	// sorts the array in descending order
+	data.sort((value1, value2) => value2.wpm - value1.wpm);
+	// writes the data to the database
+	fs.writeFileSync('./db/json/scores.json', JSON.stringify(data));
+	res.json(data);
 });
 
 // listening for requests
