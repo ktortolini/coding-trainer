1444a8cb1c1584f884c5ea86c28a1a92e6ce9191
ktortolini <134340991+ktortolini@users.noreply.github.com>
2023-09-26 21:41:25 -0500

    added for each loop that dynamically adds the scores, maximum of 10 scores

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -32,66 +32,97 @@ const signup = async (event) => {
 const showScores = async () => {
 	const response = await fetch('/api/scores');
 	const data = await response.json();
-	// accessing after
-	data.sort((a, b) => {
-		return b.wpm - a.wpm;
+	// accessing after the response and sorting
+	data.sort((value1, value2) => {
+		// assigns a weighted value for correct
+		let weightValue1 = value1.correct * 0.25;
+		let weightValue2 = value2.correct * 0.25;
+		// adds the correct values to the cpm value
+		let totalScore1 = value1.wpm + weightValue1;
+		let totalScore2 = value2.wpm + weightValue2;
+		// returns a value for sorting purposes
+		return totalScore2 - totalScore1;
 	});
 	// this is for debugging purposes
 	console.log('data', data);
 	// adds regular expressions to extract from data
 	let perMinute = new RegExp('[^0-9]*([0-9]+).*', 's');
 	let totalCorrect = new RegExp('[^0-9]*[|][^0-9]*([0-9]+)', 's');
-	// compares the regular expression to the data
-	if (data[0].user_name) {
-		try {
-			let name = JSON.stringify(data[0].user_name);
-			document.querySelector('#username1').textContent = name.slice(1, -1);
-		} catch (error) {
-			console.log(`catch (${error.stack})`);
+	// grabs the container element for dynamic creation
+	const scrollContainer = document.querySelector('.score-scroll');
+	// loops through the data to create dynamic html
+	data.forEach((item, index) => {
+		// creates the necessary article element
+		let arElement = document.createElement('article');
+		if (index + 1 === 1) {
+			arElement.classList.add('t-row', 'gold');
+		} else if (index + 1 === 2) {
+			arElement.classList.add('t-row', 'silver');
+		} else if (index + 1 === 3) {
+			arElement.classList.add('t-row', 'bronze');
+		} else if (
+			index + 1 !== 1 &&
+			index + 1 !== 2 &&
+			index + 1 !== 3 &&
+			(index + 1) % 2 === 0
+		) {
+			arElement.classList.add('t-row', 'green1');
+		} else if (
+			index + 1 !== 1 &&
+			index + 1 !== 2 &&
+			index + 1 !== 3 &&
+			(index + 1) % 2 !== 0
+		) {
+			arElement.classList.add('t-row', 'green2');
 		}
-	} else {
-		document.querySelector('#username1').textContent = `???`;
-	}
-	try {
-		document.querySelector('#cpm1').textContent = data[0].wpm;
-		document.querySelector('#correct1').textContent = data[0].correct;
-	} catch (error) {
-		console.log(`catch (${error.stack})`);
-	}
-	// compares the regular expression to the data
-	if (data[1].user_name) {
-		try {
-			let name = JSON.stringify(data[1].user_name);
-			document.querySelector('#username2').textContent = name.slice(1, -1);
-		} catch (error) {
-			console.log(`catch (${error.stack})`);
+		// creates the note element
+		let ntElement = document.createElement('note');
+		ntElement.classList.add('grid');
+		// adds the paragraph tags with a for loop
+		for (let i = 0; i < 4; i++) {
+			let pElement = document.createElement('p');
+			if (i === 0) {
+				pElement.classList.add('tt-row', `grid-item-position-${index + 1}`);
+				pElement.textContent = index + 1;
+			} else if (i === 1) {
+				pElement.classList.add('t-row', `grid-item-name-${index + 1}`);
+				pElement.id = `username${index + 1}`;
+				try {
+					let name = JSON.stringify(item.user_name);
+					// shortens the name if too long
+					let sanitizedName = name.slice(1, -1);
+					if (sanitizedName.length > 6) {
+						sanitizedName = sanitizedName.slice(0, 3) + '...';
+					}
+					pElement.textContent = sanitizedName;
+				} catch (error) {
+					console.log(`catch (${error.stack})`);
+				}
+			} else if (i === 2) {
+				pElement.classList.add('t-row', `grid-item-num-${index + 1}`);
+				pElement.id = `cpm${index + 1}`;
+				try {
+					pElement.textContent = item.wpm;
+				} catch (error) {
+					console.log(`catch (${error.stack})`);
+				}
+			} else if (i === 3) {
+				pElement.classList.add('t-row', `grid-item-score-${index + 1}`);
+				pElement.id = `correct${index + 1}`;
+				try {
+					pElement.textContent = item.correct;
+				} catch (error) {
+					console.log(`catch (${error.stack})`);
+				}
+			}
+			// appends the p element to the note
+			ntElement.appendChild(pElement);
 		}
-	} else {
-		document.querySelector('#username2').textContent = `???`;
-	}
-	try {
-		document.querySelector('#cpm2').textContent = data[1].wpm;
-		document.querySelector('#correct2').textContent = data[1].correct;
-	} catch (error) {
-		console.log(`catch (${error.stack})`);
-	}
-	// compares the regular expression to the data
-	if (data[2].user_name) {
-		try {
-			let name = JSON.stringify(data[2].user_name);
-			document.querySelector('#username3').textContent = name.slice(1, -1);
-		} catch (error) {
-			console.log(`catch (${error.stack})`);
-		}
-	} else {
-		document.querySelector('#username3').textContent = `???`;
-	}
-	try {
-		document.querySelector('#cpm3').textContent = data[2].wpm;
-		document.querySelector('#correct3').textContent = data[2].correct;
-	} catch (error) {
-		console.log(`catch (${error.stack})`);
-	}
+		// appends the note to the article
+		arElement.appendChild(ntElement);
+		// appends the article to parent element
+		scrollContainer.appendChild(arElement);
+	});
 };
 
 const signupForm = document.querySelector('.signup-form');
